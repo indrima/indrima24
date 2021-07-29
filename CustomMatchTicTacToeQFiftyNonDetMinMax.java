5f81101e639c4fe1edbc9486008a9a5f31e7ea67
indrima <56856958+indrima@users.noreply.github.com>
2021-07-29 12:32:09 -0500

    Update CustomMatchTicTacToeQFiftyNonDetMinMax.java

--- CustomMatchTicTacToeQFiftyNonDetMinMax.java
+++ CustomMatchTicTacToeQFiftyNonDetMinMax.java
@@ -31,7 +31,7 @@ public class CustomMatchTicTacToeQFiftyNonDetMinMax
 	/**
 	 * Constructor
 	 */
-	private RunCustomMatch()
+	private CustomMatchTicTacToeQFiftyNonDetMinMax()
 	{
 		// do not instantiate
 	}
